e9f727a9431ec3c9b7ed34b7201c555f237eccb2
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-07-24 21:23:47 +0600

    dashboard and min and student role base showing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello School Management!");
 });
 
 const verifyJWT = (req, res, next) => {
