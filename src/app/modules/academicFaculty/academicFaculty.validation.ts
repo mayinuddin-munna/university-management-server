a3e7913e5da7f6bd5984bc72d000746ba5b886ef
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2024-06-06 20:35:53 +0600

    fixed academic faculty validation and start academic department

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,14 +1,18 @@
 import { z } from 'zod';
 
 const createAcademicValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic faculty must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic faculty must be string',
+    }),
   }),
 });
 
 const updateAcademicValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic faculty must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic faculty must be string',
+    }),
   }),
 });
 
