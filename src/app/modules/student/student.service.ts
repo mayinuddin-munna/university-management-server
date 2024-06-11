8ff2c6dcfc7077247c0259e5a88a1dd13896c8ff
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2024-06-12 00:28:50 +0600

    nasted populate

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -2,16 +2,21 @@ import { Student } from './student.model';
 
 const getAllStudentsFromDB = async () => {
   const result = await Student.find()
-    .populate('academicDepartment')
+    .populate('admissionSemester')
     .populate({
-      path:'admissionSemester',
-      populate:{path:'academicDepartment'}
+      path: 'academicDepartment',
+      populate: { path: 'academicFaculty' },
     });
   return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-  const result = await Student.aggregate([{ $match: { id } }]);
+  const result = await Student.findById(id)
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: { path: 'academicFaculty' },
+    });
   return result;
 };
 
