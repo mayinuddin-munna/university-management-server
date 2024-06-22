61247339b53d83784b5baee566bb916e44663eac
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2024-06-22 20:41:48 +0600

    update code

--- src/app/middlewares/globalErrHandler.ts
+++ src/app/middlewares/globalErrHandler.ts
@@ -2,21 +2,50 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.stateCode || 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
 
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const errorSource: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue?.message,
+      };
+    });
+
+    return {
+      statusCode,
+      message: 'Validation error',
+      errorSource,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
+  }
+
+  // ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSource,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from 'mongoose';
 import config from '../../config';
 import { TStudent } from '../student/student.interface';
@@ -53,10 +54,11 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     await session.endSession();
 
     return newStudent;
-  } catch (error) {
+  } catch (error:any) {
     await session.abortTransaction();
     await session.endSession();
-    throw new AppError(httpStatus.NOT_ACCEPTABLE, 'Failed to create student');
+    throw new Error(error);
+    // throw new AppError(httpStatus.NOT_ACCEPTABLE, 'Failed to create student');
   }
 };
 
