03b6e299664c185a78c095c1fa5b7954a47fa172
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2024-09-09 23:32:01 +0600

    asdfasdf

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,9 +1,24 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 import { SemesterRegistration } from './semesterRegistration.model';
 
 const createSemesterRegistrationIntoDB = async (
   payload: TSemesterRegistration,
 ) => {
+  const academicSemester = payload.academicSemester;
+
+  const isAcademicSemesterExists =
+    await AcademicSemester.findById(academicSemester);
+
+  if (!isAcademicSemesterExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'this academic semester is not found!',
+    );
+  }
+
   const result = await SemesterRegistration.create(payload);
   return result;
 };
